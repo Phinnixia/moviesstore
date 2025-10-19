6edaa4728637543562cf950d003d0298bfb1b027
Nirupama Reddy <reddy.nirupama08@gmail.com>
2025-10-19 16:20:54 -0400

    ShowPopMovieHome

--- home/views.py
+++ home/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
+from cart.models import Item
+from django.db.models import Count
 
 # Create your views here.
 def index(request):
     template_data = {}
     template_data['title'] = 'Movies Store'
+    state_movie_stats = (
+        Item.objects
+        .values('order__state', 'movie__name')
+        .annotate(num_purchases=Count('id'))
+        .exclude(order__state__isnull=True)
+        .exclude(order__state__exact='')
+        .order_by('-num_purchases')
+    )
+
+    top_movie_per_state = {}
+    for entry in state_movie_stats:
+        state = entry['order__state']
+        movie = entry['movie__name']
+        if state not in top_movie_per_state:
+            top_movie_per_state[state] = movie
+
+    template_data['map_data'] = top_movie_per_state
     return render(request, 'home/index.html', {
         'template_data': template_data})
 def about(request):
