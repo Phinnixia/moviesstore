770b8ee0b5ccc1f472b5988698e04c4eb52830b7
sekun <sekunoshikanlu@gmail.com>
2025-10-19 23:38:52 -0400

    changes

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import dotenv
-
-dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
